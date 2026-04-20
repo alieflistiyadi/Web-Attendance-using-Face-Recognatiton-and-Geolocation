02aa7d270f82434b97566d03b9bddcf79bee3c8a
nathleen <putridinam687@gmail.com>
2026-04-20 23:04:02 +0700

    seach and pagination in admin page

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Siswa;
 
 class SiswaController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $siswa = \DB::table('siswa')->orderBy('nama_lengkap')
-        ->join('jurusan', 'siswa.kode_jurusan', '=', 'jurusan.kode_jurusan')
-        ->get();
-        return view('siswa.index', compact('siswa'));
+        $query = Siswa::query();
+        $query->select('siswa.*','nama_jurusan');
+        $query->join('jurusan', 'siswa.kode_jurusan', '=', 'jurusan.kode_jurusan');
+        $query->orderBy('nama_lengkap');
+        if(!empty($request->nama_lengkap)){
+            $query->where('nama_lengkap','like','%'.$request->nama_lengkap . '%');
+        }
+
+        if(!empty($request->kode_jurusan)){
+            $query->where('siswa.kode_jurusan', $request->kode_jurusan);
+        }
+        $siswa = $query->paginate(2);
+
+        $jurusan = DB::table('jurusan')->get();
+        return view('siswa.index', compact('siswa', 'jurusan'));
     }
 }
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -19,7 +19,42 @@
       <div class="col-12">
         <div class="card">
           <div class="card-body">
-            <table class="table table-bordered">
+            <div class="row">
+              <div class="col-12">
+                <form action="/siswa" method="GET">
+              <div class="row">
+                <div class="col-6">
+                  <div class="form-group">
+                    <input type="text" name="nama_lengkap" id="nama_lengkap" class="form-control" 
+                    placeholder="Nama Siswa" value="{{ Request('nama_lengkap')}}">
+                  </div>
+                </div>
+                <div class="col-4">
+                  <div class="form-group">
+                    <select name="kode_jurusan" id="kode_jurusan" class="form-select">
+                      <option value="">Jurusan</option>
+                      @foreach ($jurusan as $d)
+                          <option {{ Request('kode_jurusan')==$d->kode_jurusan ? 'selected' : ''}} value="{{ $d->kode_jurusan }}">{{ $d->nama_jurusan}}</option>
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
+                <div class="col-2">
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+                   Cari
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+              </div>
+            </div>
+
+            <div class="row mt-2">
+              <div class="col-12">
+                <table class="table table-bordered">
           <thead>
             <tr>
               <th>No</th>
@@ -38,7 +73,7 @@
                 $path = Storage::url('uploads/absensi/'.$d ->foto)
             @endphp
               <tr>
-                <td>{{ $loop->iteration }}</td>
+                <td>{{ $loop->iteration + $siswa->firstItem() -1}}</td>
                 <td>{{ $d->nis }}</td>
                 <td>{{ $d->nama_lengkap }}</td>
                 <td>{{ $d->kelas }}</td>
@@ -57,7 +92,11 @@
             @endforeach
           </tbody>
         </table>
+        {{ $siswa->links('vendor.pagination.bootstrap-5') }}
           </div>
+              </div>
+            </div>
+            
         </div>
         
 
