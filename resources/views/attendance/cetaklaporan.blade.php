5723f2f7ddab318705478edfdedbd8527f292456
fznyeach <ichanfauzan21@gmail.com>
2026-05-13 15:49:08 +0700

    update laporan attendance

--- resources/views/attendance/cetaklaporan.blade.php
+++ resources/views/attendance/cetaklaporan.blade.php
@@ -65,7 +65,22 @@
 <!-- Set "A5", "A4" or "A3" for class name -->
 <!-- Set also "landscape" if you need -->
 <body class="A4">
-
+    <?php
+    function selisih($jam_masuk, $jam_keluar)
+    {
+        list($h, $m, $s) = explode(":", $jam_masuk);
+        $dtAwal = mktime($h, $m, $s, "1", "1", "1");
+        list($h, $m, $s) = explode(":", $jam_keluar);
+        $dtAkhir = mktime($h, $m, $s, "1", "1", "1");
+        $dtSelisih = $dtAkhir - $dtAwal;
+        $totalmenit = $dtSelisih / 60;
+        $jam = explode(".", $totalmenit / 60);
+        $sisamenit = ($totalmenit / 60) - $jam[0];
+        $sisamenit2 = $sisamenit * 60;
+        $jml_jam = $jam[0];
+        return $jml_jam . ":" . round($sisamenit2);
+    }
+    ?>
   <!-- Each sheet element should have the class "sheet" -->
   <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
   <section class="sheet padding-10mm">
@@ -134,8 +149,9 @@
         </tr>
         @foreach ($attendance as $d)
                 @php
-                    $path_in = Storage::url('uploads/absensi/' .$d->foto_in);
-                    $path_out = Storage::url('uploads/absensi/' .$d->foto_out);
+                $path_in = Storage::url('uploads/absensi/' .$d->foto_in);
+                $path_out = Storage::url('uploads/absensi/' .$d->foto_out);
+                $jamterlambat = selisih('07:00:00', $d->jam_in);
                 @endphp
             <tr>
                 <td> {{ $loop->iteration }} </td>
@@ -143,10 +159,15 @@
                 <td> {{ $d->jam_in }} </td>
                 <td> <img src="{{ url($path_in) }}" class="foto" alt=""> </td>
                 <td> {{ $d->jam_out != null ? $d->jam_out : 'Belum Absen' }} </td>
-                <td> <img src="{{ url($path_out) }}" class="foto" alt""> </td>
+                <td> 
+                    @if ($d->jam_out != null)
+                    <img src="{{ url($path_out) }}" class="foto" alt=""> </td>
+                    @else
+                    <img src="{{ asset('assets/img/logocamera.jpg') }}" class="foto" alt=""> </td>
+                    @endif
                 <td>
                     @if ($d->jam_in > '07:00')
-                    Terlambat
+                    Terlambat {{ $jamterlambat }}
                     @else
                     Tepat Waktu
                     @endif
@@ -154,6 +175,18 @@
             </tr>
         @endforeach
     </table>
+
+    <table width="100%" style="margin-top:100px">
+    <tr>
+        <td style="text-align: right">
+            Cikarang, {{ date('d-m-Y') }}
+            <br><br><br><br>
+
+            <u>Nama Kepala Sekolah</u><br>
+            <i><b>Kepala Sekolah</b></i>
+        </td>
+    </tr>
+</table>
   </section>
 
 </body>
